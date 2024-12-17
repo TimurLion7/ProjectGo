8ed9522d163b8dc46be02345030e9a671ceb67a4
Timur <tima.askarov.77@gmail.com>
2024-12-17 21:09:06 +0300

    Описание коммита 2 задания

--- main.go
+++ main.go
@@ -8,26 +8,37 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type requestBody struct {
+/*type requestBody struct {
 	Message string `json:"message"`
-}
-
-var task string
+}*/
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
-	var body requestBody
+	var body Message
 	json.NewDecoder(r.Body).Decode(&body)
-	task = body.Message
-	fmt.Fprintf(w, "Message: %s updated successfully!", task)
+	// Сохраняем задачу в базу данных
+	DB.Create(&body)
+	fmt.Fprintf(w, "Received task: %s, IsDone: %t updated successfully!", body.Task, body.IsDone)
 }
 
 func GetHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s", task)
+	var tasks []Message
+	DB.Find(&tasks)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+
 }
 
 func main() {
+	// Вызываем метод InitDB() из файла db.go
+	InitDB()
+
+	// Автоматическая миграция модели Message
+	DB.AutoMigrate(&Message{})
+
 	router := mux.NewRouter()
 	router.HandleFunc("/api/hello", GetHandler).Methods("GET")
 	router.HandleFunc("/api/hello", PostHandler).Methods("POST")
+
 	http.ListenAndServe(":8080", router)
 }
