7d918c69ba411877d25c68690427d82e8f269f90
Timur <tima.askarov.77@gmail.com>
2024-12-18 12:54:15 +0300

    Описание 2 коммита 2 задания

--- main.go
+++ main.go
@@ -2,22 +2,20 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
-
-/*type requestBody struct {
-	Message string `json:"message"`
-}*/
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
 	var body Message
 	json.NewDecoder(r.Body).Decode(&body)
 	// Сохраняем задачу в базу данных
 	DB.Create(&body)
-	fmt.Fprintf(w, "Received task: %s, IsDone: %t updated successfully!", body.Task, body.IsDone)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(body)
 }
 
 func GetHandler(w http.ResponseWriter, r *http.Request) {
